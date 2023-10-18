872c5e4995518836e12632224f98da87a3f23309
hco00002@mix.wvu.edu <hco00002@mix.wvu.edu>
2023-10-17 20:06:27 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800359452</p>
+      <p>Hi I am Hunter Oxley</p>
+
+    </div>
+  );
+}
+
+export default App;
