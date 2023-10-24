046131ae55f099d1e10324e8b8214a6e7d802744
hco00002@mix.wvu.edu <hco00002@mix.wvu.edu>
2023-10-24 19:23:13 -0400

    Initial test for Cards and navbars

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar";
+import Card from "./card";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App bg-dark text white">
+      <Navbar/>
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800359452</p>
       <p>Hi I am Hunter Oxley</p>
+
+      <card/>
+      <card/>
+      <card/>
 
     </div>
   );
