ffe0f3a4f2346e8d7059c4dd8f5d6dd45cc32e5e
hco00002@mix.wvu.edu <hco00002@mix.wvu.edu>
2023-11-01 14:49:39 -0400

    More Troubleshooting for lab 7

--- Navbar.js
+++ Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//Navbar
-function Navbar() {
+
+function Navbar(props) {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -13,13 +13,13 @@ function Navbar() {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Card</a>
+          <Link class="nav-link" to="/card">Card</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <Link class="nav-link" to="/contact">Contact</Link>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" href="#">Disabled</a>
--- card.js
+++ card.js
@@ -17,7 +17,7 @@ function Card() {
       <p>I think hotdogs work well with fries</p>
     </div>
     <div className="card">
-      <h3>Card 3</h3>
+      <h3>Card 345675</h3>
       <p>Test</p>
     </div>
   </div>
